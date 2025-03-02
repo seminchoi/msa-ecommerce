bd64e612f997509c3a44153b8e785ea948086ab4
seminchoi <choicco89@gmail.com>
2025-03-03 08:49:52 +0900

    feat: 패스워드 인코딩을 위한 spring security crypto 의존성 추가

--- auth-service/build.gradle.kts
+++ auth-service/build.gradle.kts
@@ -6,5 +6,6 @@ plugins {
 
 dependencies {
 //    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.security:spring-security-crypto")
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
 }
