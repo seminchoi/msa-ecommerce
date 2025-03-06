a1360456a16c5093e90f40ced4b9ffd83677b815
seminchoi <choicco89@gmail.com>
2025-03-06 21:40:52 +0900

    feat: Product 관련 도메인에 생성자 및 getter 적용

--- product-service/product-service-domain/src/main/java/com/sem/ecommerce/product/domain/Category.java
+++ product-service/product-service-domain/src/main/java/com/sem/ecommerce/product/domain/Category.java
@@ -1,10 +1,25 @@
 package com.sem.ecommerce.product.domain;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category {
     private String UUID;
     private String name;
     private Category parent;
     private List<Category> children;
+
+    @Builder
+    public Category(String UUID, String name, Category parent, List<Category> children) {
+        this.UUID = UUID;
+        this.name = name;
+        this.parent = parent;
+        this.children = children;
+    }
 }
--- product-service/product-service-domain/src/main/java/com/sem/ecommerce/product/domain/Product.java
+++ product-service/product-service-domain/src/main/java/com/sem/ecommerce/product/domain/Product.java
@@ -1,13 +1,30 @@
 package com.sem.ecommerce.product.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+@Getter
+@NoArgsConstructor
 public class Product {
     private UUID id;
     private Category category;
     private String name;
     private String description;
     private long price;
-    private Map<String, Object> additionalAttributes;
+    private Map<String, Object> additionalAttributes = new HashMap<>();
+
+    @Builder
+    public Product(UUID id, Category category, String name, String description, long price, Map<String, Object> additionalAttributes) {
+        this.id = id;
+        this.category = category;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.additionalAttributes = additionalAttributes;
+    }
 }
