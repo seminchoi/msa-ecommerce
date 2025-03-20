02dd82e62db358ef8e6eb958b3d9247c5a495d82
seminchoi <choicco89@gmail.com>
2025-03-20 17:48:49 +0900

    test: Order Service Context Load 테스트 작성

---
+++ order/order-service/src/test/java/com/sem/ecommerce/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.sem.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+    @Test
+    public void contextLoad() {
+
+    }
+}
