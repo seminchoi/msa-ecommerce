2eeed1ed7f39b8963479b16f63302339e158b9f3
seminchoi <choicco89@gmail.com>
2025-03-25 21:02:27 +0900

    fix: publish confirm 에서 message payload를 확인할 수 없으므로 재시도로직 제거

--- order/order-service/src/main/java/com/sem/ecommerce/infra/event/RabbitConfig.java
+++ order/order-service/src/main/java/com/sem/ecommerce/infra/event/RabbitConfig.java
@@ -4,15 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.retry.backoff.ExponentialBackOffPolicy;
-import org.springframework.retry.policy.SimpleRetryPolicy;
-import org.springframework.retry.support.RetryTemplate;
 
 @Configuration
 @Slf4j
@@ -20,13 +18,8 @@ public class RabbitConfig {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    public static final String QUEUE_NAME = "my-service-queue";
-
-    @Bean
-    public Queue queue() {
-        // durable을 true로 설정하여 RabbitMQ 재시작에도 큐가 유지되도록 함
-        return new Queue(QUEUE_NAME, true, false, false);
-    }
+    @Autowired
+    private AmqpAdmin amqpAdmin;
 
     @Bean
     public ObjectMapper objectMapper() {
@@ -37,25 +30,16 @@ public class RabbitConfig {
 
     @PostConstruct
     public void init() {
+        createExchanges();
         // 메시지 확인 콜백 설정 - 브로커에게 메시지가 전달되었는지 확인
         rabbitTemplate.setConfirmCallback((correlationData, ack, cause) -> {
-            if (correlationData != null) {
-                if (ack) {
-                    log.info("Message with correlation ID: {} was acknowledged by broker",
-                            correlationData.getId());
-                } else {
-                    log.error("Message with correlation ID: {} was not acknowledged by broker. Cause: {}",
-                            correlationData.getId(), cause);
-                    String messageId = correlationData.getId();
-                    Object originalMessage = correlationData.getReturned() != null ?
-                            correlationData.getReturned().getMessage() : null;
-                    String exchangeName = correlationData.getReturned() != null ?
-                            correlationData.getReturned().getExchange() : "defaultExchange";
-                    String routingKey = correlationData.getReturned() != null ?
-                            correlationData.getReturned().getRoutingKey() : "defaultRoutingKey";
+            if (ack) {
+                log.info("Message with correlation ID: {} was acknowledged by broker",
+                        correlationData.getId());
+            } else {
+                log.error("Message with correlation ID: {} was not acknowledged by broker. Cause: {}",
+                        correlationData.getId(), cause);
 
-                    retryMessagePublish(messageId, originalMessage, exchangeName, routingKey);
-                }
             }
         });
 
@@ -69,41 +53,9 @@ public class RabbitConfig {
     }
 
 
-    private void retryMessagePublish(String messageId, Object message, String exchange, String routingKey) {
-        int maxRetries = 3;
-        long initialBackoff = 1000; // 1초
-
-        RetryTemplate retryTemplate = new RetryTemplate();
-
-        // 지수 백오프 정책 설정
-        ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
-        backOffPolicy.setInitialInterval(initialBackoff);
-        backOffPolicy.setMultiplier(2.0); // 재시도마다 대기 시간 2배 증가
-        backOffPolicy.setMaxInterval(10000); // 최대 10초
-        retryTemplate.setBackOffPolicy(backOffPolicy);
-
-        // 재시도 정책 설정
-        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
-        retryPolicy.setMaxAttempts(maxRetries);
-        retryTemplate.setRetryPolicy(retryPolicy);
-
-        try {
-            retryTemplate.execute(context -> {
-                log.info("Retrying message publish for message ID: {}. Attempt: {}",
-                        messageId, context.getRetryCount() + 1);
-
-                // 새로운 CorrelationData 생성
-                CorrelationData newCorrelationData = new CorrelationData(messageId);
-
-                // 메시지 재발행
-                rabbitTemplate.convertAndSend(exchange, routingKey, message, newCorrelationData);
-                return null;
-            });
-        } catch (Exception e) {
-            log.error("Failed to publish message after {} retries. Message ID: {}, Error: {}",
-                    maxRetries, messageId, e.getMessage());
-            // 최종 재시도 실패 후 처리 (DB에 저장하거나 알림 발송 등)
-        }
+    private void createExchanges() {
+        Exchange exchange = new TopicExchange("orders", true, false);
+        amqpAdmin.declareExchange(exchange);
     }
 
 //    @Bean
